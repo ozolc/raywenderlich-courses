6e9f6d0f8dec26173156eede0f7916ceb619c43b
Maksim Nosov <ozolc@yandex.ru>
2019-05-13 17:24:30 +0300

    Beginning Table Views · Data Sources and Delegates

---
+++ Beginning Table Views/Checklist/Checklist/TestViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TestViewController.swift
+//  Checklist
+//
+//  Created by Maksim Nosov on 13/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
+
+
+extension TestViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            switch indexPath.row {
+            case _ where indexPath.row % 5 == 0:
+                label.text = "Take a jog"
+            case _ where indexPath.row % 4 == 0:
+                label.text = "Watch a movie"
+            case _ where indexPath.row % 3 == 0:
+                label.text = "Code an app"
+            case _ where indexPath.row % 2 == 0:
+                label.text = "Walk the dog"
+            case _ where indexPath.row % 1 == 0:
+                label.text = "Study design patterns"
+            default:
+                break
+            }
+        }
+        
+        return cell
+    }
+}
+
+extension TestViewController: UITableViewDelegate {
+    
+}
