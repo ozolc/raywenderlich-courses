78cdcfcffe49372af86a38536e67e5add967edab
Maksim Nosov <ozolc@yandex.ru>
2019-05-13 15:55:10 +0300

    Beginning Table Views · Table View Controllers

---
+++ Beginning Table Views/Checklist/Checklist/ChecklistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChecklistViewController.swift
+//  Checklist
+//
+//  Created by Maksim Nosov on 13/05/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
