19c2a9d3fa35b84c2afa9dd9abf1c1848c5c4521
Maksim Nosov <ozolc@yandex.ru>
2019-05-13 16:05:39 +0300

    Beginning Table Views · Table View Cells

--- Beginning Table Views/Checklist/Checklist/ChecklistViewController.swift
+++ Beginning Table Views/Checklist/Checklist/ChecklistViewController.swift
@@ -15,6 +15,14 @@ class ChecklistViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 
 }
 
