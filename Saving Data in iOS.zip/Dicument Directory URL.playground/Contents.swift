470304581b1f0557b755ede64734c1d9a9ace5a7
Maksim Nosov <ozolc@yandex.ru>
2019-05-14 09:51:53 +0300

    Saving Data in iOS · String

--- Saving Data in iOS.zip/Dicument Directory URL.playground/Contents.swift
+++ Saving Data in iOS.zip/Dicument Directory URL.playground/Contents.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+// Bytes
+
 let mysteryBytes: [UInt8] = [
     240,          159,          152,          184,
     240,          159,          152,          185,
@@ -18,3 +20,17 @@ let savedMysteryData = try Data(contentsOf: mysteryDataURL)
 let savedMysteryBytes = Array(savedMysteryData)
 savedMysteryBytes == mysteryBytes
 savedMysteryData == mysteryData
+
+try mysteryData.write(to: mysteryDataURL.appendingPathExtension("txt"))
+
+
+// String
+
+let string = String(data: savedMysteryData, encoding: .utf8)!
+let stringURL =
+FileManager.documentDirectoryURL
+.appendingPathComponent("string")
+.appendingPathExtension("txt")
+
+try string.write(to: stringURL, atomically: true, encoding: .utf8)
+try String(contentsOf: stringURL)
