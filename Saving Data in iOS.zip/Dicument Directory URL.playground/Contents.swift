c94ec20ee4772481f36d79943b0935f0b6e8c349
Maksim Nosov <ozolc@yandex.ru>
2019-05-14 09:45:16 +0300

    Saving Data in iOS · Data

--- Saving Data in iOS.zip/Dicument Directory URL.playground/Contents.swift
+++ Saving Data in iOS.zip/Dicument Directory URL.playground/Contents.swift
@@ -1,5 +1,20 @@
 import Foundation
 
-FileManager.documentDirectoryURL
+let mysteryBytes: [UInt8] = [
+    240,          159,          152,          184,
+    240,          159,          152,          185,
+    0b1111_0000,  0b1001_1111,  0b1001_1000,  186,
+    0xF0,         0x9F,         0x98,         187
+]
 
-try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+let mysteryDataURL = URL(
+    fileURLWithPath: "mystery",
+    relativeTo: FileManager.documentDirectoryURL)
+
+let mysteryData = Data(mysteryBytes)
+try mysteryData.write(to: mysteryDataURL)
+
+let savedMysteryData = try Data(contentsOf: mysteryDataURL)
+let savedMysteryBytes = Array(savedMysteryData)
+savedMysteryBytes == mysteryBytes
+savedMysteryData == mysteryData
