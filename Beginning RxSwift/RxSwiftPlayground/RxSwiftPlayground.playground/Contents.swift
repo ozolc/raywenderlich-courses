61dc1e466c999f00afd7bde1fc9f7e6fa4aeaf56
Maksim Nosov <ozolc@yandex.ru>
2019-09-28 16:03:12 +0300

    6. Subscribing to Observables. Part 1

--- Beginning RxSwift/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ Beginning RxSwift/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -1,11 +1,64 @@
 import UIKit
 import RxSwift
 
-example(of: "createing observables") {
-    let mostPopular: Observable<String> = Observable<String>.just(episodeV)
-    let originalTrilogy = Observable.of(episodeIV, episodeV, episodeVI)
-    let prequelTrilogy = Observable.of([episodeI, episodeII, episodeIII])
-    let sequelTrilogy = Observable.from([episodeVII, episodeVIII, episodeIX])
+example(of: "subscribe") {
+    
+    let observable = Observable.of(episodeIV, episodeV, episodeVI)
+    
+    observable.subscribe(onNext: { element in
+        print(element)
+    })
 }
 
-var str = "Hello, playground"
+example(of: "empty") {
+    let observable = Observable<Void>.empty()
+    
+    observable
+        .subscribe(onNext: { element in
+            print(element)
+        },
+                   onCompleted: {
+                    print("Completed")
+        })
+}
+
+example(of: "never") {
+    let observable = Observable<Any>.never()
+    
+    observable
+        .subscribe(onNext: { element in
+            print(element)
+        },
+                   onCompleted: {
+                    print("Completed")
+        })
+}
+
+example(of: "dispose") {
+    let mostPopular = Observable.of(episodeV, episodeIV, episodeVI)
+    
+    let subscription = mostPopular.subscribe { (event) in
+        print(event)
+    }
+    
+    subscription.dispose()
+}
+
+example(of: "DisposeBag") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of(episodeVII, episodeI, rogueOne)
+        .subscribe {
+            print($0)
+    }
+    .disposed(by: disposeBag)
+}
+
+//example(of: "createing observables") {
+//    let mostPopular: Observable<String> = Observable<String>.just(episodeV)
+//    let originalTrilogy = Observable.of(episodeIV, episodeV, episodeVI)
+//    let prequelTrilogy = Observable.of([episodeI, episodeII, episodeIII])
+//    let sequelTrilogy = Observable.from([episodeVII, episodeVIII, episodeIX])
+//}
+
+
