252a73a5457c21e537b6cda62a399296b8d5ef4a
Maksim Nosov <ozolc@yandex.ru>
2019-09-28 16:28:48 +0300

    5. Subscribing to Observables. Part 2

--- Beginning RxSwift/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ Beginning RxSwift/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -54,6 +54,78 @@ example(of: "DisposeBag") {
     .disposed(by: disposeBag)
 }
 
+example(of: "create") {
+    
+    enum Droid: Error {
+        case OU812
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    Observable<String>.create { (observer) in
+        
+        observer.onNext("R2-D2")
+        observer.onError(Droid.OU812)
+        observer.onNext("C-3PO")
+        observer.onNext("K-2SO")
+        observer.onCompleted()
+        
+        return Disposables.create()
+    }
+.subscribe(
+    onNext: { print($0) },
+    onError: { print("Error:", $0) },
+    onCompleted: { print("Completed") },
+    onDisposed: { print("Disposed") }
+    )
+    .disposed(by: disposeBag)
+}
+
+example(of: "String") {
+    
+    let disposeBag = DisposeBag()
+    
+    enum FileReadError: Error {
+        case fileNotFound, unreadable, encodingFailed
+    }
+    
+    func loadText(from filename: String) -> Single<String> {
+        return Single.create { single in
+            let disposable = Disposables.create()
+            
+            guard let path = Bundle.main.path(forResource: filename, ofType: "txt") else {
+                single(.error(FileReadError.fileNotFound))
+                return disposable
+            }
+            
+            guard let data = FileManager.default.contents(atPath: path) else {
+                single(.error(FileReadError.unreadable))
+                return disposable
+            }
+            
+            guard let contents = String(data: data, encoding: .utf8) else {
+                single(.error(FileReadError.encodingFailed))
+                return disposable
+            }
+            
+            single(.success(contents))
+            
+            return disposable
+        }
+    }
+    
+    loadText(from: "ANewHope")
+        .subscribe {
+            switch $0 {
+            case .success(let string):
+                print(string)
+            case .error(let error):
+                print(error)
+            }
+    }
+    .disposed(by: disposeBag)
+}
+
 //example(of: "createing observables") {
 //    let mostPopular: Observable<String> = Observable<String>.just(episodeV)
 //    let originalTrilogy = Observable.of(episodeIV, episodeV, episodeVI)
