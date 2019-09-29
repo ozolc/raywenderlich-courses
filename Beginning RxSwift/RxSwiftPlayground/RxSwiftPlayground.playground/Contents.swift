9ae30236b7925e599cbb3373ca8b68fa8db03858
Maksim Nosov <ozolc@yandex.ru>
2019-09-29 09:45:48 +0300

    8. Subjects and Variables. Part 1

--- Beginning RxSwift/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ Beginning RxSwift/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -1,144 +1,221 @@
-import UIKit
 import RxSwift
 
-example(of: "subscribe") {
+example(of: "PublishSubject") {
     
-    let observable = Observable.of(episodeIV, episodeV, episodeVI)
+    let quotes = PublishSubject<String>()
     
-    observable.subscribe(onNext: { element in
-        print(element)
-    })
-}
-
-example(of: "empty") {
-    let observable = Observable<Void>.empty()
+    quotes.onNext(itsNotMyFault)
     
-    observable
-        .subscribe(onNext: { element in
-            print(element)
-        },
-                   onCompleted: {
-                    print("Completed")
-        })
-}
-
-example(of: "never") {
-    let observable = Observable<Any>.never()
-    
-    let disposeBag = DisposeBag()
-    
-    observable
-    .do(
-        onSubscribe: {
-            print("About to subscribe")
-    },
-        onDispose: {
-            print("Disposed")
-    })
-    .subscribe(
-        onNext: { element in
-            print(element)
-    },
-        onCompleted: {
-            print("Completed")
-    },
-        onDisposed: {
-            print("Disposed")
-    })
-    .disposed(by: disposeBag)
-}
-
-example(of: "dispose") {
-    let mostPopular = Observable.of(episodeV, episodeIV, episodeVI)
-    
-    let subscription = mostPopular.subscribe { (event) in
-        print(event)
-    }
-    
-    subscription.dispose()
-}
-
-example(of: "DisposeBag") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of(episodeVII, episodeI, rogueOne)
+    let subscriptionOne = quotes
         .subscribe {
-            print($0)
-    }
-    .disposed(by: disposeBag)
-}
-
-example(of: "create") {
-    
-    enum Droid: Error {
-        case OU812
+            print(label: "1)", event: $0)
     }
     
-    let disposeBag = DisposeBag()
+    quotes.on(.next(doOrDoNot))
     
-    Observable<String>.create { (observer) in
-        
-        observer.onNext("R2-D2")
-        observer.onError(Droid.OU812)
-        observer.onNext("C-3PO")
-        observer.onNext("K-2SO")
-        observer.onCompleted()
-        
-        return Disposables.create()
-    }
-.subscribe(
-    onNext: { print($0) },
-    onError: { print("Error:", $0) },
-    onCompleted: { print("Completed") },
-    onDisposed: { print("Disposed") }
-    )
-    .disposed(by: disposeBag)
-}
-
-example(of: "String") {
-    
-    let disposeBag = DisposeBag()
-    
-    enum FileReadError: Error {
-        case fileNotFound, unreadable, encodingFailed
-    }
-    
-    func loadText(from filename: String) -> Single<String> {
-        return Single.create { single in
-            let disposable = Disposables.create()
-            
-            guard let path = Bundle.main.path(forResource: filename, ofType: "txt") else {
-                single(.error(FileReadError.fileNotFound))
-                return disposable
-            }
-            
-            guard let data = FileManager.default.contents(atPath: path) else {
-                single(.error(FileReadError.unreadable))
-                return disposable
-            }
-            
-            guard let contents = String(data: data, encoding: .utf8) else {
-                single(.error(FileReadError.encodingFailed))
-                return disposable
-            }
-            
-            single(.success(contents))
-            
-            return disposable
-        }
-    }
-    
-    loadText(from: "ANewHope")
+    let subscriptionTwo = quotes
         .subscribe {
-            switch $0 {
-            case .success(let string):
-                print(string)
-            case .error(let error):
-                print(error)
-            }
+            print(label: "2(", event: $0)
     }
-    .disposed(by: disposeBag)
+    
+    quotes.onNext(lackOfFaith)
+    
+    subscriptionOne.dispose()
+    
+    quotes.onNext(eyesCanDevceive)
+    
+    quotes.onCompleted()
+    
+    let subscriptionThree = quotes
+        .subscribe {
+            print("3)", $0)
+    }
+    
+    quotes.onNext(stayOnTarget)
+    
+    subscriptionTwo.dispose()
+    subscriptionThree.dispose()
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//example(of: "subscribe") {
+//
+//    let observable = Observable.of(episodeIV, episodeV, episodeVI)
+//
+//    observable.subscribe(onNext: { element in
+//        print(element)
+//    })
+//}
+//
+//example(of: "empty") {
+//    let observable = Observable<Void>.empty()
+//
+//    observable
+//        .subscribe(onNext: { element in
+//            print(element)
+//        },
+//                   onCompleted: {
+//                    print("Completed")
+//        })
+//}
+//
+//example(of: "never") {
+//    let observable = Observable<Any>.never()
+//
+//    let disposeBag = DisposeBag()
+//
+//    observable
+//    .do(
+//        onSubscribe: {
+//            print("About to subscribe")
+//    },
+//        onDispose: {
+//            print("Disposed")
+//    })
+//    .subscribe(
+//        onNext: { element in
+//            print(element)
+//    },
+//        onCompleted: {
+//            print("Completed")
+//    },
+//        onDisposed: {
+//            print("Disposed")
+//    })
+//    .disposed(by: disposeBag)
+//}
+//
+//example(of: "dispose") {
+//    let mostPopular = Observable.of(episodeV, episodeIV, episodeVI)
+//
+//    let subscription = mostPopular.subscribe { (event) in
+//        print(event)
+//    }
+//
+//    subscription.dispose()
+//}
+//
+//example(of: "DisposeBag") {
+//    let disposeBag = DisposeBag()
+//
+//    Observable.of(episodeVII, episodeI, rogueOne)
+//        .subscribe {
+//            print($0)
+//    }
+//    .disposed(by: disposeBag)
+//}
+//
+//example(of: "create") {
+//
+//    enum Droid: Error {
+//        case OU812
+//    }
+//
+//    let disposeBag = DisposeBag()
+//
+//    Observable<String>.create { (observer) in
+//
+//        observer.onNext("R2-D2")
+//        observer.onError(Droid.OU812)
+//        observer.onNext("C-3PO")
+//        observer.onNext("K-2SO")
+//        observer.onCompleted()
+//
+//        return Disposables.create()
+//    }
+//.subscribe(
+//    onNext: { print($0) },
+//    onError: { print("Error:", $0) },
+//    onCompleted: { print("Completed") },
+//    onDisposed: { print("Disposed") }
+//    )
+//    .disposed(by: disposeBag)
+//}
+//
+//example(of: "String") {
+//
+//    let disposeBag = DisposeBag()
+//
+//    enum FileReadError: Error {
+//        case fileNotFound, unreadable, encodingFailed
+//    }
+//
+//    func loadText(from filename: String) -> Single<String> {
+//        return Single.create { single in
+//            let disposable = Disposables.create()
+//
+//            guard let path = Bundle.main.path(forResource: filename, ofType: "txt") else {
+//                single(.error(FileReadError.fileNotFound))
+//                return disposable
+//            }
+//
+//            guard let data = FileManager.default.contents(atPath: path) else {
+//                single(.error(FileReadError.unreadable))
+//                return disposable
+//            }
+//
+//            guard let contents = String(data: data, encoding: .utf8) else {
+//                single(.error(FileReadError.encodingFailed))
+//                return disposable
+//            }
+//
+//            single(.success(contents))
+//
+//            return disposable
+//        }
+//    }
+//
+//    loadText(from: "ANewHope")
+//        .subscribe {
+//            switch $0 {
+//            case .success(let string):
+//                print(string)
+//            case .error(let error):
+//                print(error)
+//            }
+//    }
+//    .disposed(by: disposeBag)
+//}
 
 //example(of: "createing observables") {
 //    let mostPopular: Observable<String> = Observable<String>.just(episodeV)
